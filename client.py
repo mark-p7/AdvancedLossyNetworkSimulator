4c807dfcf3f4f6eea39e31536b8f2382b597f81a
mark-p7 <markdeguzman1142@gmail.com>
2023-11-14 18:09:41 -0800

    Made changes to both client the server

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ from ipaddress import ip_address, IPv4Address, IPv6Address
 import os
 import fileinput
 
+STD_IN = sys.stdin
 SRC_IP_ADDRESS = sys.argv[1]
 SRC_PORT = sys.argv[2]
 DST_IP_ADDRESS = sys.argv[3]
@@ -16,15 +17,17 @@ STATIC_ACK = 0
 class Client:
     
     def __init__(self):
+        self.ip_address_family = ""
+        self.check_args()
         self.client_socket = socket.socket()
         self.receiving_socket = socket.socket()
-        self.ip_address_family = ""
         self.timeout = 0
         self.user_input = ""
         self.window_base = 0
         self.ack_num = 0
         self.chunks = []
-        self.check_args()
+        self.source_port = int(SRC_PORT)
+        self.destination_port = int(DST_PORT)
     
     def validate_ip(self, ip: str):
         try:
@@ -63,7 +66,7 @@ class Client:
         self.client_socket.bind((SRC_IP_ADDRESS, int(SRC_PORT)))
         
     def start_timeout(self):
-        self.timeout = time.time()
+        self.client_socket.settimeout(CLIENT_TIMEOUT)
     
     def wait_for_timeout(self):
         while time.time() - self.timeout < CLIENT_TIMEOUT:
@@ -73,10 +76,10 @@ class Client:
         self.user_input = input("Please enter the message:\n")
         print("done")
         
-    def create_header(self, seq_num, ack_num):
+    def create_header(self, seq_num: int, ack_num: int):
         size_of_data = len(self.chunks[seq_num])
-        # Header is 12 bytes long. The first 2 bytes is the size of data, the second 2 bytes is the window size, the next 4 bytes is the seq num, and the last 4 bytes is the ACK num
-        header = size_of_data.to_bytes(2, "big") + MAX_WINDOW_SIZE.to_bytes(2, "big") + seq_num.to_bytes(4, "big") + ack_num.to_bytes(4, "big")
+        # Header is 16 bytes long. 2 bytes for source port, 2 bytes for destination port, 2 bytes for size of data, 2 bytes for window size, 4 bytes for sequence number, and 4 bytes for ack number
+        header = self.source_port.to_bytes(2, "big") + self.destination_port.to_bytes(2, "big") + size_of_data.to_bytes(2, "big") + MAX_WINDOW_SIZE.to_bytes(2, "big") + seq_num.to_bytes(4, "big") + ack_num.to_bytes(4, "big")
         return header
 
     def start_client(self):
@@ -93,40 +96,53 @@ class Client:
         # Encode user input into bytes
         data_in_bytes = self.user_input.encode("utf-8")
         
-        # Split data into 12 byte chunks
-        self.chunks = [data_in_bytes[i:i+12] for i in range(0, len(data_in_bytes), 12)]
-        # self.chunks = [data_in_bytes[i:i+1012] for i in range(0, len(data_in_bytes), 1012)]
+        # Split data into 1008 byte chunks
+        self.chunks = [data_in_bytes[i:i+1008] for i in range(0, len(data_in_bytes), 1008)]
 
         # While there are still chunks to send
         while self.window_base < len(self.chunks):
-            # For each chunk in the current window
-            for i in range(self.window_base, self.window_base + MAX_WINDOW_SIZE):
-                # Check if we have reached the end of the message (the last chunk has been sent from the current window)
-                if i < len(self.chunks):
-                    # Create sequence number
-                    seq_num = i * 12
-                    # Create ack number
-                    ack_num = STATIC_ACK
-                    # Create protocol header
-                    header = self.create_header(seq_num, ack_num)
-                    # Create packet
-                    packet = header + self.chunks[i]
-                    # Send packet
-                    self.client_socket.sendto(packet, (DST_IP_ADDRESS, DST_PORT))
-                    # Log packet sent
-                    print(f"Client sent chunk with seq num {seq_num}")
-            self.start_timeout()
-            for i in range(MAX_WINDOW_SIZE):
-                # Receive packet
-                packet, _ = self.client_socket.recvfrom(12)
-                # Extract ack number from packet header
+            try:
+                # For each chunk in the current window
+                for i in range(self.window_base, self.window_base + MAX_WINDOW_SIZE):
+                    # Check if we have reached the end of the message (the last chunk has been sent from the current window)
+                    if i < len(self.chunks):
+                        # Create sequence number
+                        seq_num = i
+                        # Create ack number
+                        ack_num = STATIC_ACK
+                        # Create protocol header
+                        header = self.create_header(seq_num, ack_num)
+                        # Create packet
+                        packet = header + self.chunks[i]
+                        # Send packet
+                        self.client_socket.sendto(packet, (DST_IP_ADDRESS, int(DST_PORT)))
+                        # Log packet sent
+                        print(f"Client sent chunk with seq num {seq_num}")
+                
+                # Start timeout
+                self.start_timeout()
+                
+                # Wait for ACK
+                packet, addr = self.client_socket.recvfrom(24)
+                
+                # Extract ACK number from header
                 ack_num = int.from_bytes(packet[8:], "big")
-                # Log ack received
+                
+                # Log ACK received
                 print(f"Client received ACK for seq num {ack_num}")
-                # Slide window
-                while (ack_num / 24) > self.window_base:
-                    self.window_base = ack_num + 1
+                
+                # The ACK received is the ACK for the highest sequence number received in the current window
+                # Slide window up to the ACK received
+                self.window_base = self.ack_num + 1
+                
+            # If timeout occurs, resend all packets in the current window
+            except socket.timeout:
+                
+                # Log timeout
+                print("Timeout occurred")
             
 if __name__ == "__main__":
+    for line in STD_IN:
+        print(line)
     client = Client()
     client.start_client()
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ SRC_IP_ADDRESS = sys.argv[1]
 SRC_PORT = int(sys.argv[2])
 DST_IP_ADDRESS = sys.argv[3]
 DST_PORT = int(sys.argv[4])
+SERVER_TIMEOUT = 1
 
 def validate_ip(ip: str):
     try:
@@ -57,28 +58,97 @@ def extract(data: bytes, size: int):
     if len(data) > size:
         remaining_data = data[size:]
     return extracted_data, remaining_data
+
+class Server:
     
-def server():
+    def __init__(self) -> None:
+        pass
     
-    # Define local variables
-    ip_address_family = ""
-    current_ack = 0
-    decoded_data = ""
+    def decapsulate(self, data: bytes):
+        print("Decapsulating...")
+        if data is None:
+            print("No data to decapsulate")
+        header = data[:12]
+        payload = data[12:]
+        return header, payload
     
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_sock:
-        server_sock.bind((SRC_IP_ADDRESS, SRC_PORT))
+    def extract_header_details(self, header: bytes):
+        # Logging
+        print("Extracting header details...")
         
-        while True:
-            data, addr = server_sock.recvfrom(1024)
-            ack, data = extract(data, 4)
-            ack = decode(ack, "big-endian")
-            print(ack)
-            if ack > current_ack and ack + current_ack < 1024:
-                current_ack = ack
-                decoded_data += decode(data, "utf-8")
-            print(f"Server received: {data.decode()}")
-            # Sending ACK back to the client through the proxy
-            server_sock.sendto(ack.to_bytes(4, byteorder="big"), (DST_IP_ADDRESS, DST_PORT))
+        # Check if header is None
+        if header is None:
+            print("No header to extract")
             
+        # Extracting the header details
+        size_of_data = header[:2]
+        window_size = header[2:4]
+        seq_num = header[4:8]
+        ack_num = header[8:12]
+        
+        # Decoding the header details
+        size_of_data = decode(size_of_data, "big-endian")
+        window_size = decode(window_size, "big-endian")
+        seq_num = decode(seq_num, "big-endian")
+        ack_num = decode(ack_num, "big-endian")
+        
+        # Return the header details
+        return size_of_data, window_size, seq_num, ack_num
+    
+    def start_server(self):
+        
+        # Define local variables
+        ip_address_family = ""
+        current_ack = 0
+        decoded_data = ""
+        
+        # Create the server socket
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_sock:
+            # Bind Socket
+            server_sock.bind((SRC_IP_ADDRESS, SRC_PORT))
+            
+            # Logging
+            print("Server is listening...")
+            
+            # While the server is running
+            while True:
+                
+                # Receive data from the client through the proxy or client
+                data, addr = server_sock.recvfrom(1024)
+                
+                # Separate each packet from the data
+                for i in range(0, len(data)):
+                    size_of_data = data[i:i+2]
+                    
+                    packet = data[i:i+1024]
+                    print(f"Packet: {packet}")
+                
+                # Decapsulate the data from the packet
+                header, payload = self.decapsulate(data)
+                
+                # Extract the header details
+                size_of_data, window_size, seq_num, ack_num = self.extract_header_details(header)
+                
+                # Log the header details
+                print(f"size_of_data: {size_of_data}")
+                print(f"window_size: {window_size}")
+                print(f"seq_num: {seq_num}")
+                print(f"ack_num: {ack_num}")
+                
+                
+                
+                
+                
+                # ack, data = extract(data, 4)
+                # ack = decode(ack, "big-endian")
+                # print(ack)
+                # if ack > current_ack and ack + current_ack < 1024:
+                #     current_ack = ack
+                #     decoded_data += decode(data, "utf-8")
+                # print(f"Server received: {data.decode()}")
+                # # Sending ACK back to the client through the proxy
+                # server_sock.sendto(ack.to_bytes(4, byteorder="big"), (DST_IP_ADDRESS, DST_PORT))
+                
 if __name__ == "__main__":
-    server()
+    server = Server()
+    server.start_server()
